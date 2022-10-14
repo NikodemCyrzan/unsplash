7f7b324b714be6087738bfe19a2fa6b02c52c086
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2022-10-14 11:30:20 +0200

    added modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import "./modal.css";
+
+/**
+ * @param {{isOpen: boolean, setOpen: (value: boolean) => void, photoData: object}}
+ */
+export function Modal({isOpen, setOpen, photoData}){
+    if (photoData != null)
+        return (
+            <div className="modal__wrapper" active={isOpen.toString()}>
+                <div className="modal__background" onClick={() => setOpen(false)}></div>
+                <div className="modal__container">
+                    <div className="close" onClick={() => setOpen(false)}>
+                        <FontAwesomeIcon icon={faXmark} />
+                    </div>
+                    <div className="image__container">
+                        <img src={photoData.urls.regular} />
+                    </div>
+                    <div className="image-data__container">
+                        <div className="data-section"><span>Author:</span><span>{photoData.user.name}</span></div>
+                    </div>
+                    <div className="image-data__container">
+                        <div className="data-section"><span>Location:</span><span>{photoData.location.country}{photoData.location.city != null ? `, ${photoData.location.city}` : ""}</span></div>
+                    </div>
+                </div>
+            </div>
+        );
+}
--- src/sites/Photos.jsx
+++ src/sites/Photos.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { json, useParams } from "react-router-dom";
 import { keys } from "../api/keys";
 import { SearchBar } from "../components/SearchBar";
+import { Modal } from "../components/Modal";
 import "./photos.css";
 
 export function Photos(){
     const {query} = useParams();
     const [photos, setPhotos] = useState([]);
+
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [modalData, setModalData] = useState(null);
 
     useEffect(() => {
         fetch(`https://api.unsplash.com/search/photos/?client_id=${keys.access}&query=${query}&per_page=40`, {
@@ -15,8 +19,19 @@ export function Photos(){
         .then(json => setPhotos(json.results));
     }, []);
 
+    const img = e => <img onClick={() => {
+        fetch(`https://api.unsplash.com/photos/${e.id}?client_id=${keys.access}`, {
+            method: "GET"
+        }).then(res => res.json())
+        .then(json => {
+            setModalData(json)
+            setModalOpen(true);
+        });
+    }} key={e.id} title={e.description} src={e.urls.regular}/>;
+
     return (
     <>
+        <Modal isOpen={isModalOpen} setOpen={setModalOpen} photoData={modalData} />
         <div className="photos-topbar__wrapper">
             <SearchBar value={query} />
         </div>
@@ -25,7 +40,7 @@ export function Photos(){
             {
                 photos.map((e, i) => {
                     if (i % 3 === 0)
-                        return <img key={e.id} title={e.description} src={e.urls.regular}/>
+                        return img(e);
                     return null;
                 })
             }
@@ -34,7 +49,7 @@ export function Photos(){
             {
                 photos.map((e, i) => {
                     if ((i + 1) % 3 === 0)
-                        return <img key={e.id} title={e.description} src={e.urls.regular}/>
+                        return img(e);
                     return null;
                 })
             }
@@ -43,7 +58,7 @@ export function Photos(){
             {
                 photos.map((e, i) => {
                     if ((i + 2) % 3 === 0)
-                        return <img key={e.id} title={e.description} src={e.urls.regular}/>
+                        return img(e);
                     return null;
                 })
             }
